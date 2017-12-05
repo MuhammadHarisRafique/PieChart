16aee977237fe96b99329a32217476db89981289
anas <anas.ejaz1991@gmail.com>
2017-12-05 18:51:14 +0500

    make pie chart

--- Pie_Chart_View/ViewController.swift
+++ Pie_Chart_View/ViewController.swift
@@ -7,17 +7,205 @@
 //
 
 import UIKit
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ChartViewDelegate {
 
+
+    @IBOutlet var pieChartView: PieChartView!
+    let months = ["20-03-17", "Feb", "Mar", "Apr", "May"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0]
+    let unitsBought = [10.0, 14.0, 60.0, 13.0, 2.0]
+    @IBOutlet weak var barChartView: BarChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
+       
+        self.PieChart()
+       // self.barChart()
+        barChartView.delegate = self
+        barChartView.noDataText = "You need to provide data for the chart."
+        barChartView.chartDescription?.text = "sales vs bought "
+        
+        
+        //legend
+        let legend = barChartView.legend
+        legend.enabled = true
+        legend.horizontalAlignment = .right
+        legend.verticalAlignment = .top
+        legend.orientation = .vertical
+        legend.drawInside = true
+        legend.yOffset = 10.0;
+        legend.xOffset = 10.0;
+        legend.yEntrySpace = 0.0;
+        
+        
+        let xaxis = barChartView.xAxis
+        
+        xaxis.drawGridLinesEnabled = true
+        xaxis.labelPosition = .bottom
+        xaxis.centerAxisLabelsEnabled = true
+        xaxis.valueFormatter = IndexAxisValueFormatter(values:self.months)
+        xaxis.granularity = 1
+        
+        
+        let leftAxisFormatter = NumberFormatter()
+        leftAxisFormatter.maximumFractionDigits = 1
+        
+        let yaxis = barChartView.leftAxis
+        yaxis.spaceTop = 0.35
+        yaxis.axisMinimum = 0
+        yaxis.drawGridLinesEnabled = false
+        
+        barChartView.rightAxis.enabled = false
+        //axisFormatDelegate = self
+        
+        setChart()
+        
     }
+    
+    func PieChart() {
+        
+        let ys1 = Array(1..<10).map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) * 100.0 }
+        
+        let yse1 = ys1.enumerated().map { x, y in return PieChartDataEntry(value: y, label: String(x)) }
+        
+        let data = PieChartData()
+        let ds1 = PieChartDataSet(values: yse1, label: "Hello")
+        
+        ds1.colors = ChartColorTemplates.vordiplom()
+        
+        data.addDataSet(ds1)
+        
+        let paragraphStyle: NSMutableParagraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        paragraphStyle.alignment = .center
+        let centerText: NSMutableAttributedString = NSMutableAttributedString(string: "Charts\n by Rehan")
+        
+        
+        self.pieChartView.centerAttributedText = centerText
+        
+        self.pieChartView.data = data
+        
+        self.pieChartView.chartDescription?.text = "Piechart Demo"
 
+    }
+    
+    func barChart()  {
+        // Do any additional setup after loading the view.
+        let xArray = Array(1..<10)
+        let ys1 = xArray.map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) }
+        let ys2 = xArray.map { x in return cos(Double(x) / 2.0 / 3.141) }
+        
+        let yse1 = ys1.enumerated().map { x, y in return BarChartDataEntry(x: Double(x), y: y) }
+        let yse2 = ys2.enumerated().map { x, y in return BarChartDataEntry(x: Double(x), y: y) }
+        
+        let data = BarChartData()
+        let ds1 = BarChartDataSet(values: yse1, label: "Hello")
+        ds1.colors = [NSUIColor.red]
+        data.addDataSet(ds1)
+        
+        let ds2 = BarChartDataSet(values: yse2, label: "World")
+        ds2.colors = [NSUIColor.blue]
+        data.addDataSet(ds2)
+        
+        let barWidth = 0.4
+        let barSpace = 0.05
+        let groupSpace = 0.1
+        
+        data.barWidth = barWidth
+        self.barChartView.xAxis.axisMinimum = Double(xArray[0])
+        self.barChartView.xAxis.axisMaximum = Double(xArray[0]) + data.groupWidth(groupSpace: groupSpace, barSpace: barSpace) * Double(xArray.count)
+        // (0.4 + 0.05) * 2 (data set count) + 0.1 = 1
+        data.groupBars(fromX: Double(xArray[0]), groupSpace: groupSpace, barSpace: barSpace)
+        
+        self.barChartView.data = data
+        
+        self.barChartView.gridBackgroundColor = NSUIColor.white
+        
+        self.barChartView.chartDescription?.text = "Barchart Demo"
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+         self.pieChartView.animate(xAxisDuration: 0.0, yAxisDuration: 1.0)
+         self.barChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func setChart() {
+        
+        barChartView.noDataText = "You need to provide data for the chart."
+        var dataEntries: [BarChartDataEntry] = []
+        var dataEntries1: [BarChartDataEntry] = []
+        
+        for i in 0..<self.months.count {
+            
+            let dataEntry = BarChartDataEntry(x: Double(i) , y: self.unitsSold[i])
+            dataEntries.append(dataEntry)
+            
+            let dataEntry1 = BarChartDataEntry(x: Double(i) , y: self.self.unitsBought[i])
+            dataEntries1.append(dataEntry1)
+            
+            //stack barchart
+            //let dataEntry = BarChartDataEntry(x: Double(i), yValues:  [self.unitsSold[i],self.unitsBought[i]], label: "groupChart")
+            
+            
+            
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Unit sold")
+        let chartDataSet1 = BarChartDataSet(values: dataEntries1, label: "Unit Bought")
+        
+        let dataSets: [BarChartDataSet] = [chartDataSet,chartDataSet1]
+        chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+        //chartDataSet.colors = ChartColorTemplates.colorful()
+        //let chartData = BarChartData(dataSet: chartDataSet)
+        
+        let chartData = BarChartData(dataSets: dataSets)
+        
+        
+        let groupSpace = 0.3
+        let barSpace = 0.05
+        let barWidth = 0.3
+        // (0.3 + 0.05) * 2 + 0.3 = 1.00 -> interval per "group"
+        
+        let groupCount = self.months.count
+        let startYear = 0
+        
+        
+        chartData.barWidth = barWidth;
+        barChartView.xAxis.axisMinimum = Double(startYear)
+        let gg = chartData.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+        print("Groupspace: \(gg)")
+        barChartView.xAxis.axisMaximum = Double(startYear) + gg * Double(groupCount)
+        
+        chartData.groupBars(fromX: Double(startYear), groupSpace: groupSpace, barSpace: barSpace)
+        //chartData.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+        barChartView.notifyDataSetChanged()
+        
+        barChartView.data = chartData
+        
+        
+        
+        
+        
+        
+        //background color
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+        
+        //chart animation
+        barChartView.animate(xAxisDuration: 1.5, yAxisDuration: 1.5, easingOption: .linear)
+        
+        
     }
 
 
